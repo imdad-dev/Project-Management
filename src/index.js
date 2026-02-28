b8ff77619bf8bf772d55667f9d4e779eaa5bd41e
unknown <imdadulhoquekk@gmail.com>
2026-03-01 03:56:36 +0530

    Setup express basic server and Test API via postman

--- src/index.js
+++ src/index.js
@@ -21,11 +21,31 @@ console.log("Username : " , myUserName);
 app.use(express.json());
 
 
-app.get("/" , async (req , res)=>{
+app.get("/" ,  (req , res)=>{
    res.status(200).send("Hello from Server")
 })
 
 
+app.get("/home" , (req , res)=>{
+
+   res.status(200).send("<p>This is Home Page!</p>");
+})
+
+app.get("/task" , (req , res)=>{
+
+   res.status(200).json({ "task1" : "Write here your first Task"})
+})
+
+
+app.get("/facebook" , (req , res)=>{
+
+   res.status(200).redirect("https://facebook.com");
+})
+
+app.get("/end" , (req , res)=>{
+
+   res.status(400).end();
+})
 
 app.listen(port , ()=>{
 console.log(`Server is lisnetng at http://localhost:${port}`);
