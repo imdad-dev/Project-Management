42ac2f61e80b1f3ab750b3556c68f09a063e0a49
unknown <imdadulhoquekk@gmail.com>
2026-03-08 05:41:54 +0530

    keeping data in  constants file in utiles because  may be change data in future , based on that we take certain action

---
+++ src/utils/constant.js
@@ -0,0 +1,17 @@
+export const userRoleEnum = {
+    ADMIN : "admin",
+    PROJECT_ADMIN : "project_admin" ,
+    MEMBER : "member"
+}
+
+export const availableUserRole = Object.values(userRoleEnum);
+
+export const taskStatusEnum = {
+   TODO : "todo" ,
+   IN_PROGRESS : "in_progress" ,
+   DONE  : "done" 
+}
+
+export const  availableTaskStatus = Object.values(taskStatusEnum);
+
+
