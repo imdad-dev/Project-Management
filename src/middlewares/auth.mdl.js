063656ab71dc720bb4b1215cfe654fea447f002d
unknown <imdadulhoquekk@gmail.com>
2026-03-11 23:58:45 +0530

    Added jwt verify token middlewares

---
+++ src/middlewares/auth.mdl.js
@@ -0,0 +1,33 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async_handler.js";
+import jwt from "jsonwebtoken";
+
+/** =============== Verify JWT Token ================== */
+export const verifyJWT = asyncHandler(async (req , res, next)=>{
+    // get encoded token
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace('Bearer ' , "");
+
+    if(!token) {
+        throw new ApiError(401, "Unauthorized request");
+    }
+    
+    try {
+        
+     const decodedToken =  jwt.verify(token , process.env. ACCESS_TOKEN_SECRET);
+ const user = await User.findById(decodedToken?._id).select(
+    "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+ )
+
+   if(!user) {
+        throw new ApiError(401, "Invalid Access token");
+    }
+    req.user = user;
+    next();
+
+    } catch (error) {
+         throw new ApiError(401, "Invalid Access token");
+        
+    }
+
+})
