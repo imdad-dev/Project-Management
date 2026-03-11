4b3f3b543afc68f8b37b439be4b226cb5dbd5c8f
unknown <imdadulhoquekk@gmail.com>
2026-03-11 16:22:44 +0530

    Added in model Generate Access token and Refresh token logic using jwt and hide secret .env file

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose , { Schema} from "mongoose";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
 
 const userSchema = new Schema( { 
@@ -85,6 +86,35 @@ userSchema.pre("save" , async function (next){
 userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password , this.password);
 }
+
+/* ---------------------Genrate Access Token --------------------- */
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign( { 
+        _id : this._id,
+        email : this.email,
+        username : this.username,
+    } , 
+    process.env.ACCESS_TOKEN_SECRET  ,
+
+    { expiresIn : process.env.ACCESS_TOKEN_EXPIRY    }
+ )
+}
+
+
+
+/* ---------------------Genrate Refresh Token --------------------- */
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign( { 
+        _id : this._id,
+        email : this.email,
+       
+    } , 
+    process.env.REFRESH_TOKEN_SECRET  ,
+
+    { expiresIn : process.env.REFRESH_TOKEN_EXPIRY    }
+ )
+}
+
 
 
 export const  User = mongoose.model("user" , userSchema);
