ae4bd560c1e7316f24142cfa13391d184af209f4
unknown <imdadulhoquekk@gmail.com>
2026-03-17 21:50:23 +0530

    Added Login user with token and cookies controller

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express"
-import {registerUser} from  "../controller/auth.controller.js"
+import {registerUser ,loginUser} from  "../controller/auth.controller.js"
+import { userRegisterValidator } from "../validators/auth.js"
+import{ validate} from "../middlewares/validator.middleware.js"
 
 const router = Router()
 
 router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
 
 
 export default router;
